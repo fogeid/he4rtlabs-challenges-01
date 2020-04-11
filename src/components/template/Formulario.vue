0f89cfb605c278a4c5cec250a3367b3fa589360c
Diego Batista <diego.silva@aluno.ic.ufmt.br>
2020-04-11 13:18:18 -0400

    adc formulario

--- src/components/template/Formulario.vue
+++ src/components/template/Formulario.vue
@@ -1,6 +1,18 @@
 <template>
     <div>
-        
+        <form class="form">
+            <label class="form__label" for="tempo-diario">Tempo Diário</label>
+            <input class="form__input" type="text" id="diaria">
+            <label class="form__label" for="tempo-diario">Dias Efetivos</label>
+            <input class="form__input" type="text" id="efetivos">
+            <label class="form__label" for="tempo-diario">Dias de Férias</label>
+            <input class="form__input" type="text" id="ferias">
+            <label class="form__label" for="tempo-diario">Valor Total do Projeto</label>
+            <input class="form__input" type="text" id="valor-total">
+            <div class="form__btn">
+                <button class="btn" type="submit">Calcular</button>
+            </div>
+        </form>
     </div>    
 </template>
 
@@ -10,6 +22,6 @@ export default {
 }
 </script>
 
-<style lang="scss">
+<style src="./formulario.scss" lang="scss">
 
 </style>
--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -2,20 +2,25 @@
   <div id="app">
     <header class="header">
       <app-logo />
-      <app-social-midia />
-    </header>    
+      <app-social-midia />      
+    </header>
+    <section>
+      <app-form />
+    </section>
   </div>
 </template>
 
 <script>
 import Logo from '@/components/template/Logo.vue';
 import Social from '@/components/template/Social.vue';
+import Formulario from '@/components/template/Formulario.vue';
 
 export default {
   name: 'Home',
   components: {
     'app-logo': Logo,
     'app-social-midia': Social,
+    'app-form': Formulario,
   }
 }
 </script>
